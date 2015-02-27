00a760977eaf47ca3e8ef8061892a8819f7588b1
Adrien Jonquet <adrien.jonquet@gmail.com>
2015-02-27 11:02:25 +0100

    add thenAndError promise

--- src/angular/Promise.hx
+++ src/angular/Promise.hx
@@ -5,5 +5,7 @@ extern class Promise<T> {
 	@:native("then")
 	public function thenFlat<U>(f : T -> Promise<U>) : Promise<U>;
 
+	@:native("then")
+	public function thenAndError<U>(f : T -> U, err : Dynamic -> U) : Promise<U>;
 	public function then<U>(f : T -> U) : Promise<U>;
 }
