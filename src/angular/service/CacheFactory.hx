11fa0b580440aa4f95d6c8a64582e82d8382b5fa
JJayet <jonathan@jayet.eu>
2015-03-23 11:03:58 +0100

    Added  definition

--- src/angular/service/CacheFactory.hx
+++ src/angular/service/CacheFactory.hx
@@ -1,9 +1,27 @@
 
 package angular.service;
 
+typedef CacheOptions = {
+	capacity : Int
+}
+
 @:injectionName("$cacheFactory")
 extern class CacheFactory {
+	@:selfCall public function apply(cacheName : String, ?options : CacheOptions) : Cache;
 
+ 	public function get<T>(cacheId : String) : Null<Cache>;
+ 	public function info() : Dynamic<CacheInfo>;
+}
 
+typedef CacheInfo = {
+	id : String,
+	size : Int
+}
 
+extern class Cache {
+	public function put(key : String, value : String) : Void;
+	public function info() : CacheInfo;
+	public function remove(key : String) : Void;
+	public function removeAll() : Void;
+	public function destroy() : Void;
 }
